d00c4f4849f61cd2abc9799da3ebe84097e84bf4
nja93 <njanjalchege@gmail.com>
2023-02-16 16:22:35 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includes <stdio.h>
+/**
+ * main - Entry point prints a line of code with puts function
+ *
+ * Return:0
+*/
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
