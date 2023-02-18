65621698e27b9d4d1e1045ff95896a1ff47c4c39
nja93 <njanjalchege@gmail.com>
2023-02-17 16:54:58 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: code that computes int of a between 0 and 10
+ *
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+/* start of code block */
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+/* code that computes int of a between 0 and 10 */
+	return (0);
+}
