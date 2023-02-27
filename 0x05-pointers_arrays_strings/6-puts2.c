88072a8995564ca659f4207400459986226ea9c1
nja93 <njanjalchege@gmail.com>
2023-02-27 14:44:47 -0800

    Write a function that prints every other character of a string, starting with the first character,

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * puts2 - function that prints every other character of a string
+ * put2 - function that prints every other character of a string
  * @str: str parameter
  */
 
@@ -11,7 +11,7 @@ void put2(char *str)
 	int len, i;
 
 	len = strlen(str);
-	for (i = 0; i < len; i += 2) /* skip by 2 characters*/
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
