8a0bcfb6708d98a1059132009e6031402af171bf
nja93 <njanjalchege@gmail.com>
2023-03-10 21:17:32 +0000

    c files

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Computes the lenght of a character
+ * @s: string pointer passed to this function
+ * Return: returns the length of the string
+ */
+
+int _strlen(char *s)
+{
+		return (strlen(s));
+}
