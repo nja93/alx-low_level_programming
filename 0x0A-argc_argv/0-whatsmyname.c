d57848d6d54fb55215dac2cb730fef6401a2f369
nja93 <njanjalchege@gmail.com>
2023-03-11 11:20:05 +0000

    If you rename the program, it will print the new name, without having to compile it again, You should not remove the path before the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main _ Print the name of theprogram
+ * @argc: Count arguments
+ * @arcgv: Arguments
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
