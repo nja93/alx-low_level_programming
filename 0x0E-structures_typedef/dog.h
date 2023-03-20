f772fbef81f3ebf197e16e5e5771e96c7d096e5a
nja93 <njanjalchege@gmail.com>
2023-03-20 20:36:28 +0000

    Define a new type struct dog with the following elements: name, type = char *, age, type = float, owner, type = char *

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,8 @@ struct dog
 	char *owner;
 };
 /**
- *  * dog_t - new name (typedef) for dog struct.
- *   */
+* dog_t - new name (typedef) for dog struct.
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
