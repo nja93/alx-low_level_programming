386d1445055d8103184775d14a7f9b705a050be6
nja93 <njanjalchege@gmail.com>
2023-03-23 12:44:15 +0000

    function that prints strings, followed by a new line, where separator is the string to be printed between the strings, and n is the number of strings passed to the function, allowed to use printf, separator is NULL, don’t print it, If one of the string is NULL, print (nil) instead, Print a new line at the end of your function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,14 +1,11 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 /**
  * print_strings - function that prints strings, followed by a new line
  * @separator: string to be printed between strings
  * @n: number of strings passed to the functions
  * @...: Avariable number of strings to be printed
- *
- *
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
