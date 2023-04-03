cd7d8120a8ec53abbac433670c3a995de81ecdb8
nja93 <njanjalchege@gmail.com>
2023-04-03 11:05:33 +0000

    function that returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,11 @@ size_t listint_len(const listint_t *h)
 {
 	 size_t count = 0;
 
-	  while (h)
-		   {
-			    count++;
-			     h = h->next;
-			      }
-
-	   return (num);
+	while (h)
+		{
+		count++;
+		h = h->next;
+		}
+	return (count);
 }
 
