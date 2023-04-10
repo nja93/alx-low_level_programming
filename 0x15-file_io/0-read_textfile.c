f391fef5cb80d8fb0be3fbdf2d535828f7f323c9
nja93 <njanjalchege@gmail.com>
2023-04-10 21:58:50 +0000

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * read_textfile - fn that reads a text file and prints it to the POSIX standard outpu
+ * read_textfile - fn that reads a text file and prints it to the STDOUT
  * @letters: number of letters it should read and print
  * Return: number of letters it can read and print
  * if the file can not be opened or read, return 0
+ * @filename : pointer to the name of the file
  * if filename is NULL return 0
  * if write fails or does not write the expected amount of bytes, return 0
  * by chege
@@ -12,8 +13,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	int chege, length, i, result
-	char *buffer
+	int chege, length, i, result;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0); /* check if file is NULL */
