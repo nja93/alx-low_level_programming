32f91a351d9fa41adf5e0d73fec784a64143f2fb
nja93 <njanjalchege@gmail.com>
2023-04-10 23:57:28 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,11 +26,10 @@ int create_file(const char *filename, char *text_content)
 	if (chege == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
-	length = strlen(text_content);
-
+	if (text_content != NULL)
+	{	for(length = 0; text_content[length];)
+			length++;
+	}
 	status = write(chege, text_content, length);
 	if (status == -1 || status == -1)
 		return (-1);
