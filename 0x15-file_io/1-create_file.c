56fb13a82e73b02009a26dfaec44ee9f078308ec
nja93 <njanjalchege@gmail.com>
2023-04-10 23:45:42 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,9 +4,9 @@
  * create_file - function that creates a file.
  * @filename: name of the file
  * where filename is the name of the file to create and
- * @text_content: NULL terminated string to write to the file
- * Return: 1 on success, -1 on failurei
- * .. (file can not be created, file can not be written, write “fails”,
+ * @text_content:  a NULL terminated string to write to the file
+ * Return: 1 (Successful), -1 on failure.
+ * file can not be created, file can not be written, write “fails”,
  * The created file must have those permissions: rw-------.
  * If the file already exists, do not change the permissions.
  * if the file already exists, truncate it
