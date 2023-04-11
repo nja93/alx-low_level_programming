0419608ca84d58fa90a4bdf46077d13ba1af5d1e
nja93 <njanjalchege@gmail.com>
2023-04-11 01:47:56 +0000

    function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (-1);
 
-	chege = open(filename, O_APPEND | O_WRONLY);
+	chege = open(filename, O_WRONLY | O_APPEND);
 	if (chege == -1)
 		return (-1);
 
