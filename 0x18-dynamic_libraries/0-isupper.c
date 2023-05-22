f4263a687b51787500e1a0b02f4c13d62f7649c3
nja93 <njanjalchege@gmail.com>
2023-05-22 17:27:41 +0000

    dynamic library libdynamic.so containing all the functions listed in main.h

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -Check if a letter is upper
+ * @x: The numbber to be checked
+ * Return: 1 for upper letter of 0 for any other
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - joins two strings with n  bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of character to be copied
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
