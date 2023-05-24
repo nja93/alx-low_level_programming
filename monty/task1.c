691734240f0aae8106102e583c4acd761ea8fa01
nja93 <njanjalchege@gmail.com>
2023-05-24 18:48:49 +0000

    first attempt task 1

--- monty/task1.c
+++ monty/task1.c
@@ -3,18 +3,26 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "monty.h"
-int main (int argc, char* argv[])
+/**
+ * main - fn that
+ * @argc: Number of arguments
+ * @argv: Actual arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		printf("USAGE: monty file\n");
+		printf("USAGE: monty argv[%s]\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	FILE *file = fopen(argv[1], "r");
-	if (file == NULL)
+		if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file stackbyte.m");
 		exit(EXIT_FAILURE);
 	}
+return (0);
 }
